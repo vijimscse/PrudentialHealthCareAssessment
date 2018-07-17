36690848fc7a88338e947d46d93dbb1539f1b388
VijayaLakshmi.IN <vijimscse@gmail.com>
2018-07-17 12:45:47 +0530

    Login Page basic version

--- app/src/main/java/com/prudential/assessment/LoginActivity.kt
+++ app/src/main/java/com/prudential/assessment/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.prudential.assessment
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
